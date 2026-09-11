87f0b1f14b405782fa8e837adc3217ce52dc5b2b
ghoshsayani45 <ghosh.sayani45@gmail.com>
2026-09-11 11:03:12 +0530

    Added deployment code

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,17 +1,30 @@
-import json
+#import json
+import os
 import redis
-from app.core.config import settings
+#from app.core.config import settings
+from dotenv import load_dotenv  # added for render deployment
 
-redis_client = redis.Redis.from_url(settings.REDIS_URL)
+load_dotenv()  # Load environment variables from .env file
+
+REDIS_URL = os.getenv("REDIS_URL")  # Get the Redis URL from environment variables
+
+#redis_client = redis.Redis.from_url(settings.REDIS_URL)
+
+# Use the Redis URL from environment variables (added for render deployment)
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)  
 
 def get_cached_prediction(key: str):
     """
     Retrieve the cached prediction for the given input data from Redis.
     """
     value = redis_client.get(key)
-    if value:
-        return json.loads(value)
-    return None
+    # if value:
+    #     return json.loads(value)
+    # return None
+
+    return eval(value) if value else None  # Use eval to convert string back to dictionary
 
 def set_cached_prediction(key: str, value: dict, expiry: int = 3600):
-    redis_client.setex(key, expiry, json.dumps(value))
+    #redis_client.setex(key, expiry, json.dumps(value))
+
+    redis_client.set(key,str(value))
